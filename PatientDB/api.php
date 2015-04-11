9b0bdbacfad0affba4172372a4e244275db22498
msnyder102 <msnyder102@gmail.com>
2015-04-11 15:53:45 -0500

    New DB + updated API

--- PatientDB/api.php
+++ PatientDB/api.php
@@ -4,50 +4,132 @@ class API
 	public $db;
 	function __construct()
 	{
-		$this->db = mysqli_connect('localhost','root','',"patient_database"); 
+		$this->db = mysqli_connect('localhost','root','',"pdb"); 
 	}
 	function __destruct()
 	{
 		mysqli_close($this->db);
 	}
-
 	/**
-	 * @url GET getPatient/:name
+	 * @url GET getPatientFromName/:first_name/:last_name
 	 */
-	function getPatient($name)
+	function getPatientFromName($first_name, $last_name)
 	{
+		//not sure if we actually want to offer this function, first+last name is not unique unlike SSN/PatID
+		//usage should be: like query on names then get patient from id
 		$sql = <<<SQL
 		SELECT *
 		FROM `patients`
-		WHERE `Name` = '$name'
+		WHERE `First_Name` = '$first_name'
+		AND `Last_Name` = '$last_name'
 SQL;
 		if($result = $this->db->query($sql)){
-			return $result->fetch_object();
+			if($result->num_rows === 0)
+				return json_encode(array('Error' => 'No Match'));
+			else
+				return $result->fetch_object();
 		}
 		else{
-			die('There was an error running the query [' . $this->db->error . ']');
+			return json_encode(array('Error' => 'Database Error [' . $this->db->error . ']'));
 		}
 	}
 	
 	/**
-	 * @url GET getPatientsLike/:name
+	 * @url GET getPatientFromID/:patID
 	 */
-	function getPatientsLike($name)
+	function getPatientFromID($patID)
 	{
 		$sql = <<<SQL
 		SELECT *
 		FROM `patients`
-		WHERE `Name` LIKE '%$name%'
+		WHERE `patient_ID` = '$patID'
 SQL;
 		if($result = $this->db->query($sql)){
-			$jsonArray = array();
-			while ($patient = $result->fetch_array(MYSQL_ASSOC)) {
-				$jsonArray[] = $patient['Name'];
-			}
-			echo json_encode($jsonArray);
+			if($result->num_rows === 0)
+				return json_encode(array('Error' => 'No Match'));
+			else
+				return $result->fetch_object();
 		}
 		else{
-			die('There was an error running the query [' . $this->db->error . ']');
+			return json_encode(array('Error' => 'Database Error [' . $this->db->error . ']'));
+		}
+	}
+	
+	/**
+	 * @url GET getPatientFromSSN/:SSN
+	 */
+	function getPatientFromSSN($SSN)
+	{
+		$sql = <<<SQL
+		SELECT *
+		FROM `patients`
+		WHERE `SSN` = '$SSN'
+SQL;
+		if($result = $this->db->query($sql)){
+			if($result->num_rows === 0)
+				return json_encode(array('Error' => 'No Match'));
+			else
+				return $result->fetch_object();
+		}
+		else{
+			return json_encode(array('Error' => 'Database Error [' . $this->db->error . ']'));
+		}
+	}
+	
+	/**
+	 * @url GET getPatientsLike/:first_name/:last_name
+	 */
+	function getPatientsLike($first_name, $last_name)
+	{
+		$sql = <<<SQL
+		SELECT *
+		FROM `patients`
+		WHERE `First_Name` LIKE '$first_name%'
+		AND `Last_Name` LIKE '$last_name%'
+SQL;
+		if($result = $this->db->query($sql)){
+			
+			if($result->num_rows === 0)
+				return json_encode(array(['Error' => 'No Match']));
+			else
+			{
+				$jsonArray = array();
+				while ($patient = $result->fetch_array(MYSQL_ASSOC)) {
+					$jsonArray[] = array('First_Name' => $patient['First_Name'], 'Last_Name' => $patient['Last_Name'], 'Patient_ID' => $patient['Patient_ID']);
+				}	
+				return json_encode($jsonArray);
+			}
+		}
+		else{
+			return json_encode(array('Error' => 'Database Error [' . $this->db->error . ']'));
+		}
+	}
+	
+	/**
+	 * @url GET getPatientsLikeSSN/:SSN
+	 */
+	function getPatientsLikeSSN($SSN)
+	{
+		$sql = <<<SQL
+		SELECT *
+		FROM `patients`
+		WHERE `SSN` LIKE '$SSN%'
+SQL;
+		if($result = $this->db->query($sql)){
+			
+			if($result->num_rows === 0)
+				return json_encode(array(['Error' => 'No Match']));
+			else
+			{
+				$jsonArray = array();
+				while ($patient = $result->fetch_array(MYSQL_ASSOC)) {
+					$jsonArray[] = array('First_Name' => $patient['First_Name'], 'Last_Name' => $patient['Last_Name'], 'Patient_ID' => $patient['Patient_ID']);
+				}	
+				return json_encode($jsonArray);
+			}
+		}
+		else{
+			return json_encode(array('Error' => 'Database Error [' . $this->db->error . ']'));
 		}
 	}
 	
@@ -61,14 +143,19 @@ SQL;
 		FROM `patients`
 SQL;
 		if($result = $this->db->query($sql)){
-			$jsonArray = array();
-			while ($patient = $result->fetch_array(MYSQL_ASSOC)) {
-				$jsonArray[] = $patient['Name'];
+			if($result->num_rows === 0)
+				return json_encode(array('Error' => 'No Match'));
+			else
+			{
+				$jsonArray = array();
+				while ($patient = $result->fetch_array(MYSQL_ASSOC)) {
+					$jsonArray[] = array('First_Name' => $patient['First_Name'], 'Last_Name' => $patient['Last_Name'], 'Patient_ID' => $patient['Patient_ID']);
+				}
+				return json_encode($jsonArray);
 			}
-			echo json_encode($jsonArray);
 		}
 		else{
-			die('There was an error running the query [' . $this->db->error . ']');
+			return json_encode(array('Error' => 'Database Error [' . $this->db->error . ']'));
 		}
 	}
 	
@@ -79,18 +166,23 @@ SQL;
 	{
 		$sql = <<<SQL
 		SELECT *
-		FROM `records`
-		WHERE `Patient ID` = $patID
+		FROM `visits`
+		WHERE `Patient_ID` = $patID
 SQL;
 		if($result = $this->db->query($sql)){
-			$jsonArray = array();
-			while ($record = $result->fetch_array(MYSQL_ASSOC)) {
-				$jsonArray[] = $record;
+			if($result->num_rows === 0)
+				return json_encode(array('Error' => 'No Match'));
+			else
+			{
+				$jsonArray = array();
+				while ($record = $result->fetch_array(MYSQL_ASSOC)) {
+					$jsonArray[] = $record;
+				}
+				return json_encode($jsonArray);
 			}
-			echo json_encode($jsonArray);
 		}
 		else{
-			die('There was an error running the query [' . $this->db->error . ']');
+			return json_encode(array('Error' => 'Database Error [' . $this->db->error . ']'));
 		}
 	}
 }
